6ddcc51d44ebd8f39fc0abc6b3619a90788e417f
warungman <isna11583@gmail.com>
2014-05-13 10:51:29 +0700

    update files

---
+++ hello.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+#call module
+from time import time, ctime
+
+def helloWorld():
+    return "Hello World"
+
+def getTimestamp():
+    return int(time())
+
+def getCurrentTime():
+    return ctime()
